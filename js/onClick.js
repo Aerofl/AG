7a1daf952c40e7c09b7f3e474e66577d61329109
Hassanean Albaker <halbaker@q2ebanking.com>
2014-05-20 10:13:37 -0500

    animation change, added autoplay, hid gif/text on videoplay

--- js/onClick.js
+++ js/onClick.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 	$('.vid1').click(function(){
 		$(this).toggleClass("open-vid");
-		// $(this).animate({width:'700px'},200);
+		$(this).toggleClass("animate");
+		$(".corporate").show();
+		$("#video1")[0].play();
+		$(".vid2, .vid3, .reelgif, .reeltext").hide();
+	});
+	$('.vid2').click(function(){
+		$(this).toggleClass("open-vid");
+		$(this).toggleClass("animate");
+		$("commercial").show();
+		$("#video2")[0].play();
+		$(".vid1, .vid3, .reelgif, .reeltext").hide();
+	});
+	$('.vid3').click(function(){
+		$(this).toggleClass("open-vid");
+		$(this).toggleClass("animate");
+		$("narrative").show();
+		$("#video3")[0].play();
+		$(".vid1, .vid2, .reelgif, .reeltext").hide();
 	});
 });
