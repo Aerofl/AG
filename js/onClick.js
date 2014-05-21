1d8cf24517ec5da8ece777addab5a7167e9b3060
Hassanean Albaker <halbaker@q2ebanking.com>
2014-05-21 16:33:19 -0500

    Animation redesign overhaul, dynamic video display loaded

--- js/onClick.js
+++ js/onClick.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+  $('.vid1').hide().fadeIn(800);
+  $('.vid2').hide().fadeIn(1200);
+  $('.vid3').hide().fadeIn(1600);
   $('.vid-init').on('click', function (evt) {
     var $target = $(evt.currentTarget),
         $video = $('<video controls></div>'),
         $source = $(document.createElement('source')),
         videoSrc = $target.data('src'),
-        $this = $(this)
+        $this = $(this),
+        dynwidth = $(window).width(),
+        dynheight = $(window).height();
 
     evt.preventDefault();
 
@@ -15,20 +20,22 @@ $(document).ready(function(){
       .attr('type', 'video/mp4');
     $video
       .append($source)
-      .addClass('video-js vjs-default-skin corporate')
+      .addClass('video-js vjs-default-skin reels')
       .attr('preload', 'auto')
-      .attr('width', 600)
-      .attr('height', 250)
-      .data('setup', '{"example_operation": true}')
+      .height(dynheight)
+      .width(dynwidth)
+      .data('setup', '{"example_operation": true}');
+    $video[0].play();
+    $('.video-modal').fadeIn("slow");
 
-    $(".video-modal").show();
+    $(".video-modal, .close").show();
     $('.video-container').append($video);
     $('.reeltext, .reelgif').hide();
+    $('.close').show();
   });
   $('.video-modal').on('click', function (evt) {
     var $target = $(evt.target),
         $vidInit = $('.vid-init');
-
     if ($target.hasClass('video-modal') || $target.parent().hasClass('video-modal')) {
       $('.video-modal .video-js').remove();
       $('.reeltext, .reelgif').show();
